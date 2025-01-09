1f43966a1348a2ae0c8a54f3014cc4f1a458d59b
AishShrestha <shresthaayush49@gmail.com>
2025-01-09 19:34:15 +0545

    Refactor: added additional imports and exports

--- src/component/index.js
+++ src/component/index.js
@@ -1,5 +1,10 @@
-import Header from './Header/Header'
-import Footer from './Footer/Footer'
-import Container from './Container/Container'
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./Container/Container";
+import Input from "./Input";
+import Button from "./Button";
+import Signup from "./Signup";
+import RTE from "./RTE";
+import Select from "./Select";
 
-export { Header,Footer,Container}
+export { Header, Footer, Container, Input, Button, Signup, RTE, Select };
